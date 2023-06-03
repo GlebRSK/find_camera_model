2ccce318f891df83b014c1599891d8763a3b8297
glebRSK <gleb172v@gmail.com>
2023-06-03 09:33:05 +0300

    read exif data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,32 @@
+extern crate exif;
+
 use clap::{arg, Command};
+use exif::{Tag, In};
+
+
+
+pub fn exif_reader(filename: &String) -> Result<(), exif::Error> {
+    println!("{filename}");
+    
+    let exif_tags = [Tag::Model];
+    
+    for path in [filename] {    
+        let file = std::fs::File::open(path)?;
+        let mut bufreader = std::io::BufReader::new(&file);
+        let exifreader = exif::Reader::new();
+        let exif = exifreader.read_from_container(&mut bufreader)?;
+        
+        for &tag in exif_tags.iter() {
+            if let Some(field) = exif.get_field(tag, In::PRIMARY) {
+                let f = field.display_value().with_unit(&exif).to_string();
+                println!("{f}");
+            }
+        }
+    }
+
+    Ok(())
+}
+
 
 fn main() {
     let matches = Command::new("read metadata")
@@ -8,5 +36,5 @@ fn main() {
 
     let directory = matches.get_one::<String>("directory").expect("required");
 
-    println!("{directory}")
+    exif_reader(directory);
 }
